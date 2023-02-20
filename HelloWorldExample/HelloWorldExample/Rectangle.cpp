6a3d6a697add1e3617d344fd19d5af9b65302f13
ctchartier <ctchartier@wpi.edu>
2023-02-20 15:50:15 -0500

    Set up for collab changes

---
+++ HelloWorldExample/HelloWorldExample/Rectangle.cpp
@@ -0,0 +1,14 @@
+#include "Rectangle.h"
+
+//Nate
+Rectangle::Rectangle(float width = 0, float height = 0) {
+
+}
+//Nate
+Rectangle::~Rectangle() {
+
+}
+//Nate
+float Rectangle::getArea() const{
+
+}
